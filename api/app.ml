47fcd607d8714a810bed51cffec2b0f55e9eb464
Patrick Ferris <patrick@sirref.org>
2022-03-03 13:04:57 +0000

    Clear underlying git store on fresh pulls

--- api/app.ml
+++ api/app.ml
@@ -18,22 +18,38 @@ end
 
 module Gql = Irmin_graphql.Server.Make (Cohttp_lwt_unix.Server) (Config) (Store)
 
-let rec rmrf path = match Sys.is_directory path with
-  | true ->
-    Sys.readdir path |>
-    Array.iter (fun name -> rmrf (Filename.concat path name));
-    Unix.rmdir path
-  | false -> Sys.remove path
+let store_location = "./repos"
 
-let repo () =
-  let config = Irmin_git.config ~bare:true "./repos" in
-  Store.Repo.v config
+let config = Irmin_git.config ~bare:true store_location
 
+(* There must only exist a single repository that we manipulate in order for
+   Grahpql schema to work. *)
+let repo () = Store.Repo.v config
+
+let check_status = 
+  Dream.log "Sync status: %a" (Fmt.result ~ok:Sync.pp_status ~error:Sync.pp_pull_error)
+
+(* Here we clear the underlying git store in preparation for a [Sync.pull], this ensures
+   the repository is empty before the pull otherwise if a different remote is given it will
+   not work. *)
+let clear_repo () =
+  let* store = Store.Git.v (Fpath.v store_location) in
+    match store with
+    | Ok t -> 
+      let* status = Store.Git.reset t in
+      Dream.log "Reset status: %a" (Fmt.result ~ok:(Fmt.any "success") ~error:Store.Git.pp_error) status;
+      Lwt.return_unit
+    | Error err -> 
+      Dream.log "Store error: %a" Store.Git.pp_error err;
+      failwith "err"
+
+(* Syncing resets the repository and pulls in the new data from the specified remote. *)
 let sync repo data = 
-  rmrf "./repos";
-  let remote = Store.remote data in
+  let* () = clear_repo () in
   let* t = Store.master repo in
-  let* _ = Sync.pull_exn t remote `Set in 
+  let remote = Store.remote data in
+  let* status = Sync.pull t remote `Set in
+  check_status status;
   let+ readme = Store.get t [ "README.md" ] in
   Printf.printf "%s\n%!" readme;
   "Done"
